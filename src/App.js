d8685d7db8220efc9b2216930f640e157c38ca21
codingccarpenter <christineassists@aol.com>
2023-02-20 17:07:27 -0600

    independant exercise to create directory

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Directory from './components/directory/directory.component';
 import './categories.styles.scss';
-import CategoryItem from './components/category-item/category-item.component';
 
 function App() {
   console.log ("App")
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
-    }
-  ]
 
   return (
-    <div className="categories-container">
-      {categories.map((category) => (
-        <CategoryItem category={category} />
-      ))}
-
-    </div>
+    <Directory />
   );
 }
 
