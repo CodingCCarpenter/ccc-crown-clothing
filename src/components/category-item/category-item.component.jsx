d8967d08033a95b3b932890259b2e31e58590714
codingccarpenter <christineassists@aol.com>
2023-02-20 15:38:29 -0600

    destructure category and remove ID from root div of component

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import './category-item.styles.scss';
 
 const CategoryItem = ({category}) => {
+    const {imageUrl, title} = category;
+    
     return(
-        <div className="category-container" id={category.id}>
+        <div className="category-container">
           <div className="background-image" style={{
-            backgroundImage: `url(${category.imageUrl})`
+            backgroundImage: `url(${imageUrl})`
           }}/> 
           <div className="category-body-container">
-            <h2>{category.title}</h2>
+            <h2>{title}</h2>
             <p>Shop Now</p>
           </div>
         </div>
