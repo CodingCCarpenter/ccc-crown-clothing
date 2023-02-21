25c2016123b64eade06a9db16c8fb97280bad8e2
codingccarpenter <christineassists@aol.com>
2023-02-21 05:30:10 -0600

    specify exact paths for existing routes and add a new component Navigation, and add to route

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './routes/home/home.component';
 
+
+const Navigation = () => {
+  return(
+    <div>
+      <div>
+        <h1>I am the navigation bar</h1>
+      </div>
+    </div>
+  )
+}
 const App = () => {
   console.log ("App")
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/'element={<Navigation />} />
+      <Route exact path='/' element={<Home />} />
       <Route path='/home' element={<Home />} />
     </Routes>
   );
